23ae87dbac27766dafc9ae134473be4f19655b1a
Oseloka I <115153490+oselokaiweka@users.noreply.github.com>
2024-06-26 06:55:58 +0000

    Modifiedd the docker-compose.yml, dockerfile and settings.py file to utilize a postgres db for the django app.

--- dockerfile
+++ dockerfile
@@ -1,12 +1,12 @@
 FROM python:3.10-slim-buster
 
-WORKDIR /app
+WORKDIR /realynx_container_root
 
-COPY requirments.txt /app/
+COPY requirements.txt /realynx_container_root/
 
 RUN pip install --upgrade pip && pip install -r requirements.txt
 
-COPY . /app/
+COPY . /realynx_container_root/
 
 ENV DJANGO_SETTINGS_MODULE=property_webapp_project.settings
 
--- property_webapp_project/settings.py
+++ property_webapp_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "property_webapp_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "realynx"
+        "PASSWORD": "realynx_"
+        "HOST": "db"
+        "PORT": 5432
     }
 }
 
